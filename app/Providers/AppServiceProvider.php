f5cbf36e757fec25bee8745aa3ed17e413d8e6f1
Kelvin <175104668@qq.com>
2019-10-05 16:01:44 +0800

    显示个人资料

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        //log 日志中 记录执行的 SQL 语句
+        \DB::listen(function ($query) {
+            $tmp = str_replace('?', '"'.'%s'.'"', $query->sql);
+            $qBindings = [];
+            foreach ($query->bindings as $key => $value) {
+                if (is_numeric($key)) {
+                    $qBindings[] = $value;
+                } else {
+                    $tmp = str_replace(':'.$key, '"'.$value.'"', $tmp);
+                }
+            }
+            $tmp = vsprintf($tmp, $qBindings);
+            $tmp = str_replace("\\", "", $tmp);
+            \Log::info(' execution time: '.$query->time.'ms; '.$tmp."\n\n\t");
+
+        });
     }
 }
