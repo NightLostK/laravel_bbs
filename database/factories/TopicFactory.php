f0ac8b4fbc5cf20a79106838961113e434dc59df
Kelvin <175104668@qq.com>
2019-10-07 14:26:56 +0800

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,20 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    // 创建时间不能超过 $updated_at
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
