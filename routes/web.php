814fae4e57beed8da0365eb520786a71a6e9b585
Kelvin <175104668@qq.com>
2019-11-19 16:01:06 +0800

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
 // 话题相关路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 // 话题分类列表页面
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
